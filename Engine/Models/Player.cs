8e199bdac56801baa0149185f44fb8202ac4da83
jmbinamira <jmlbinamira@gmail.com>
2023-11-30 11:44:54 +0800

    first commit

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    internal class Player
+    {
+        string Name { get; set; }
+        string CharClass { get; set; }
+        double Health { get; set; }
+        double ExperiencePoints { get; set; }
+        int Level { get; set; }
+        int Septim {  get; set; }
+
+    }
+}
