1c1fcf036779b48735bda5728b3b9cbf7cceb389
jmbinamira <jmlbinamira@gmail.com>
2023-11-30 17:30:39 +0800

    first commit

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Engine.Models;  //Gets Player() class on ./Models/Player.cs
+using Engine.Models;  //Gets Player(), Location() class on ./Models/Player.cs
 
 namespace Engine.ViewModels
 {
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+
+        public Location CurrentLocation { get; set; }
 
         public GameSession()  //Constructor (if we want to create a GameSession() object)
         {
@@ -20,6 +22,13 @@ namespace Engine.ViewModels
             CurrentPlayer.Gold = 0;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 0;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "Your home";
+            CurrentLocation.ImageName = "pack://application:,,,/Engine;component/Images/Locations/home.png";  //Modified Engine.csproj by adding .WindowsDesktop at ProjectSdk
         }
     }
 }
