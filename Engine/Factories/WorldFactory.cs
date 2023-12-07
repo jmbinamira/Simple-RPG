e54ea0e0dbf637b0d57ae292ea5315aebd31ef6a
jmbinamira <jmlbinamira@gmail.com>
2023-12-07 10:52:40 +0800

    added new locations

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -17,13 +17,37 @@ namespace Engine.Factories
                "Full of crops and other farm plants.",
                "pack://application:,,,/Engine;component/Images/Locations/farmfield.png");
 
-            newWorld.AddLocation(-1, -1, "Farmhouse",
+            newWorld.AddLocation(-1, -1, "Farm House",
                 "Area where farm animals go for shelter.",
                 "pack://application:,,,/Engine;component/Images/Locations/farmhouse.png");
 
             newWorld.AddLocation(0, -1, "Home", 
                 "Your home", 
-                "pack://application:,,,/Engine;component/Images/Locations/home.png");                                                                                       
+                "pack://application:,,,/Engine;component/Images/Locations/home.png");
+
+            newWorld.AddLocation(0, 0, "Town Center",
+                "Main social hub of the town.",
+                "pack://application:,,,/Engine;component/Images/Locations/towncenter.png");
+
+            newWorld.AddLocation(-1, 0, "Merchant Store",
+                "Local store where all kinds of items are traded.",
+                "pack://application:,,,/Engine;component/Images/Locations/merchantstore.png");
+
+            newWorld.AddLocation(0, 1, "Alchemy Store",
+                "Where an alchemist/wizard resides to sell tomes and potions.",
+                "pack://application:,,,/Engine;component/Images/Locations/alchemystore.png");
+
+            newWorld.AddLocation(0, 2, "Alchemist's Field",
+                "Full of colorful plants, mushrooms, and critters.",
+                "pack://application:,,,/Engine;component/Images/Locations/alchemyfield.png");
+
+            newWorld.AddLocation(1, 0, "Town Gate",
+                "The only thing that protects the city from the outside world.",
+                "pack://application:,,,/Engine;component/Images/Locations/towngate.png");
+
+            newWorld.AddLocation(2, 0, "Haunted Mound",
+                "Otherworldly and demonic creatures roam free in this area.",
+                "pack://application:,,,/Engine;component/Images/Locations/hauntedmound.png");
 
             return newWorld;
         }
